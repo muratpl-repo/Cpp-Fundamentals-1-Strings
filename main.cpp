f2559c2b442e841fd574649f287cf22b18ffda54
muratpala007 <muratpala007@gmail.com>
2022-04-19 13:28:29 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+    string isim;
+    string soyisim;
+    string meslek;
+    int yas;
+
+    cout << "Lutfen isim, soyisim, meslek ve yas degerini giriniz..."<<endl;
+    cin>>isim>>soyisim>>meslek>>yas;
+    cout<<"Kullanici bilgileri: "<<isim<<" "<<soyisim<<" "<<meslek<<" "<<yas;*/
+
+
+    string isimler[3] = {"ahmet","mehmet","mustafa"};
+
+    cout<<isimler[0]<<endl;
+    cout<<isimler[1]<<endl;
+    cout<<isimler[2]<<endl;
+
+    return 0;
+}
